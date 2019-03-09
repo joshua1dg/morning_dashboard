011bbe99c9427bd948a7d4e772238aba68b94cb5
Joshua1gd <joshua1gd@yahoo.com>
2019-03-09 10:34:56 -0800

    transferred type info and apicallreducer file

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,6 +3,7 @@ export default {
     GET_AGENDA: 'get_agenda',
     GET_QUOTE: 'get_quote',
     GET_RSS: 'get_rss',
-    GET_YOUTUBE_INFO:'get_youtube_info'
+    GET_YOUTUBE_INFO:'get_youtube_info',
+    SEND_USER_INFO: 'send_user_info'
 }
 
--- src/reducers/api_call_reducer.js
+++ src/reducers/api_call_reducer.js
@@ -1,6 +1,7 @@
 import types from '../actions/types'
 
-const DEFAULT_STATE={
+const DEFAULT_STATE = {
+    formInfoStored: false,
     longitude: null,
     latitude: null,
     todayWeatherObj: [],
@@ -11,19 +12,21 @@ const DEFAULT_STATE={
     youtubeInfo: null
 }
 
-export default (state=DEFAULT_STATE, action) => {
-    switch(action.type){
-    case types.GET_WEATHER:
-        return {...state, ...action.payload};
-    case types.GET_AGENDA:
-        return {...state, ...action.payload}
-    case types.GET_QUOTE:
-        return {...state, ...action.payload}
-    case types.GET_RSS:
-        return {...state, ...action.payload}
-    case types.GET_YOUTUBE_INFO:
-        return {...state, ...action.payload}
-    default:
-        return state;   
+export default (state = DEFAULT_STATE, action) => {
+    switch (action.type) {
+        case types.GET_WEATHER:
+            return { ...state, ...action.payload };
+        case types.GET_AGENDA:
+            return { ...state, ...action.payload }
+        case types.GET_QUOTE:
+            return { ...state, ...action.payload }
+        case types.GET_RSS:
+            return { ...state, ...action.payload }
+        case types.GET_YOUTUBE_INFO:
+            return { ...state, ...action.payload }
+        case types.SEND_USER_INFO:
+            return { ...state, ...action.payload }
+        default:
+            return state;
     }
 }
