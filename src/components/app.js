181a30a5c4f81384a57a51023c4027b3afe3e50f
Joshua1gd <joshua1gd@yahoo.com>
2019-03-20 13:27:49 -0700

    added homepage redirect to credentials

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import Dashboard from './dashboard';
 import Submit from '../components/general/forms/submit';
 import SignIn from '../components/general/forms/signIn'
@@ -13,6 +13,7 @@ class App extends Component{
             // <Submit/>
             // </Fragment>
             <Switch>
+                <Route exact path='/' render={()=>(<Redirect to='/credentials'/>)} />
                 <Route path='/signin' component={SignIn}/>
                 <Route path='/credentials' component={Submit}/>
                 <Route path='/home' component={Dashboard}/>
